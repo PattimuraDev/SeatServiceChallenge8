ce0801ec0ec14bfdb7e7fb19c9ffaadfe636d19a
PattimuraDev <dwisatriapatra2000@gmail.com>
2022-12-28 17:13:02 +0700

    Update swagger server list

--- src/main/java/org/example/configuration/SwaggerConfig.java
+++ src/main/java/org/example/configuration/SwaggerConfig.java
@@ -3,9 +3,13 @@ package org.example.configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * kelas untuk representasi konfigurasi swagger ui
@@ -24,6 +28,11 @@ public class SwaggerConfig {
     public OpenAPI demoApi(@Value("REST API seat microservice for challenge chapter 7 backend java Binar Academy") String appDescription,
                            @Value("v1.0.0") String appVersion
     ) {
+        Server server = new Server();
+        server.setUrl("https://seat-microservice-challenge8.up.railway.app/");
+        List<Server> listOfServer = new ArrayList<>();
+        listOfServer.add(server);
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Seat Microservice Endpoints")
@@ -34,6 +43,6 @@ public class SwaggerConfig {
                                 .name("Apache 2.0")
                                 .url("http://springdoc.org")
                         )
-                );
+                ).servers(listOfServer);
     }
 }
